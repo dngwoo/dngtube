c63d335acadb6fc90b6e4389c7c4e2ec6b91f538
dngwoo <akfmzk84@naver.com>
2020-10-17 18:08:00 +0900

    page 셋팅

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = 3065;
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT} Open!`);
